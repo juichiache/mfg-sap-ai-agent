24a0e72166257b5e904ecb09b0e9d27c26faffcb
Brandon Campbell <brandon@LAPTOP-M68UHIVJ.>
2025-04-24 08:13:07 -0400

    added devtunnel to the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
+# Install Azure CLI
+# https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+# Install Azure Dev CLI
+# https://learn.microsoft.com/en-us/azure/developer/dev-cli/install-azd
 curl -fsSL https://aka.ms/install-azd.sh | bash
 
+# Install vim
 sudo apt install vim
 
+# Install Terraform
+# https://learn.microsoft.com/en-us/azure/developer/terraform/install-terraform
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
@@ -19,3 +26,9 @@ sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update
 sudo apt-get install -y terraform
+
+# Install DevTunnel CLI
+# https://learn.microsoft.com/en-us/azure/developer/devtunnel/cli-install
+curl -sL https://aka.ms/DevTunnelCliInstall | bash
+
+sudo apt update -y && sudo apt upgrade -y
